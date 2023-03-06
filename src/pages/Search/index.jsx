988ef90c3362fc4ea2d6b1d4a7997c2e23c902f3
lucastenani <lucas.tenani3@gmail.com>
2023-03-06 19:15:26 -0300

    Creating the search screen

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,5 +1,41 @@
+import { useSearchParams } from "react-router-dom";
+import "./style.scss";
+import { useEffect, useState } from "react";
+
+const searchURL = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Search = () => {
-  return <div>Search</div>;
+  const [searchParams] = useSearchParams();
+
+  const [movies, setMovies] = useState([]);
+  const query = searchParams.get("q");
+
+  const getSearchedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setMovies(data.results);
+  };
+
+  useEffect(() => {
+    const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`;
+    getSearchedMovies(searchWithQueryURL);
+  }, [query]);
+
+  console.log(movies);
+
+  return (
+    <div className="container">
+      <h2 className="title">
+        Results for: <span className="query-text">{query}</span>
+      </h2>
+      <div className="movies-container">
+        {/* {movies.length === 0 && <p>Loading...</p>}
+        {movies.length > 0 &&
+          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)} */}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
