9997830f0215fbff1aeb3890ea4b58f22096ca08
lucastenani <lucas.tenani3@gmail.com>
2023-03-06 15:17:01 -0300

    Turning the navbar into a component to get more organized

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <div className="App">
-      <nav id="navbar">
-        <h2>
-          <Link to="/">MoviesLib</Link>
-        </h2>
-
-        <Link to="/movie/1">Movie</Link>
-        <Link to="/search">Search</Link>
-      </nav>
+      <Navbar />
 
       <Outlet />
     </div>
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/">MoviesLib</Link>
+      </h2>
+
+      <Link to="/movie/1">Movie</Link>
+      <Link to="/search">Search</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
