1f6c59e8fef2a7a41cba9cf9c8cb8d6ec2cad7ac
lucastenani <lucas.tenani3@gmail.com>
2023-03-06 15:07:22 -0300

    Importing all the components that will be used in the react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Search from "./pages/Search";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
