eaa0d83367e4be90350b9e708b0bc16e37cc1240
lucastenani <lucas.tenani3@gmail.com>
2023-03-06 18:02:51 -0300

    Removing import from react as it is no longer needed

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const moviesURl = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
