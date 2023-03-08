c811e9945c371d5cf13b26aef56c00f700cb3912
lucastenani <lucas.tenani3@gmail.com>
2023-03-07 23:32:04 -0300

    removing console.log

--- src/pages/Movie/index.jsx
+++ src/pages/Movie/index.jsx
@@ -19,7 +19,6 @@ const Movie = () => {
   const getMovie = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
     setMovie(data);
   };
 
