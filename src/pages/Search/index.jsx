d22bcab010fbb0c793cde76bb010ec2a34c173a4
lucastenani <lucas.tenani3@gmail.com>
2023-03-07 23:10:06 -0300

    Fixing search logic so it works perfectly

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,28 +1,30 @@
 import { useSearchParams } from "react-router-dom";
-import "./style.scss";
 import { useEffect, useState } from "react";
+import MovieCard from "../../components/MovieCard";
+import "./style.scss";
 
-const searchURL = import.meta.env.VITE_SEARCH;
+const searchUrl = import.meta.env.VITE_SEARCH;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const Search = () => {
   const [searchParams] = useSearchParams();
 
   const [movies, setMovies] = useState([]);
+
   const query = searchParams.get("q");
 
   const getSearchedMovies = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
+
     setMovies(data.results);
   };
 
   useEffect(() => {
-    const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`;
-    getSearchedMovies(searchWithQueryURL);
-  }, [query]);
+    const searchWithQueryUrl = `${searchUrl}?${apiKey}&query=${query}`;
 
-  console.log(movies);
+    getSearchedMovies(searchWithQueryUrl);
+  }, [query]);
 
   return (
     <div className="container">
@@ -30,9 +32,9 @@ const Search = () => {
         Results for: <span className="query-text">{query}</span>
       </h2>
       <div className="movies-container">
-        {/* {movies.length === 0 && <p>Loading...</p>}
+        {movies.length === 0 && <p>Loading...</p>}
         {movies.length > 0 &&
-          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)} */}
+          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
     </div>
   );
